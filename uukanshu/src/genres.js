74327666d240198be76c95f0753bd755a31b8e60
TuanHai03 <157023052+TuanHai03@users.noreply.github.com>
2024-10-30 22:03:33 +0700

    Add files via upload

--- uukanshu/src/genres.js
+++ uukanshu/src/genres.js
@@ -1,29 +1,20 @@
 load("config.js");
-function execute(url, page) {
-    if (!page) page = '1';
-    let response = fetch( BASE_URL+url);
-
-    
-
-   if (response.ok) {
-        
+function execute(input, page) {
+    let response = fetch( BASE_URL+input); 
+   if (response.ok) {        
         let doc=response.html();
-        let next=doc.select(".next").first().attr("href")
         let keywords=doc.select(".bookbox");
         console.log( keywords);
         let books=[]
-        keywords.forEach(book => {
-           
-            
-            books.push({
-                
+        keywords.forEach(book => {                      
+            books.push({                
                 name: book.select(".bookname").text(),
                 link: book.select(".bookname a").first().attr("href"),
                 description: book.select(".author .del_but").text().replace("作者：", ""),
                 host: BASE_URL
             })
         });
-        return Response.success(books, next);
+        return Response.success(books);
     }
     return null;
 }
