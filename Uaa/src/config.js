85eec36d6c4271b065f63c3e02632a4290fdc2ab
TuanHai03 <157023052+TuanHai03@users.noreply.github.com>
2024-11-06 23:10:23 +0700

    Add files via upload

--- Uaa/src/config.js
+++ Uaa/src/config.js
@@ -1,4 +1,4 @@
-1let BASE_URL = 'https://www.uaa.com';
+let BASE_URL = 'https://www.uaa.com';
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
--- Uaa/src/toc.js
+++ Uaa/src/toc.js
@@ -1,27 +1,28 @@
-load('config.js');
+load('config.js')
 
 function execute(url) {
     let response = fetch(url, {
         headers: {
             "token": "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6ODYwNzIxNzA0MDMxMzU4OTc2LCJ0eXBlIjoiY3VzdG9tZXIiLCJ0aW1lc3RhbXAiOjE2ODUzNzg1MTE1NzQsImV4cCI6MTY4NTk4MzMxMX0.-FX7rOJP7I10ApjeM5NVaGj57aeYnkVyopniC7U_Dv8"
         }
-    });
+    });let data=[]
     if (response.ok) {
 
         let doc = response.html();
         //console.log(json)
-        let data=[]
-   let chapters=doc.select(".catalog_box .catalog_ul li")
-    chapters.forEach(e => {
+        
+        let chapters=doc.select(".catalog_box .catalog_ul li")
+        chapters.forEach(e => {
         console.log(e)
         data.push({
             name: e.select("a").first().text(),
             url: e.select("a").first().attr("href"),
             
         })
-    });
-        return Response.success(data);
         
+    })
+        
+        return Response.success(data);
        
     }
     return null;
