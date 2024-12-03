1e9d898dde4a1eb5c19e6dd77d465024b477e55f
TuanHai03 <157023052+TuanHai03@users.noreply.github.com>
2024-12-03 19:40:47 +0700

    Add files via upload

--- 69shu/src/toc.js
+++ 69shu/src/toc.js
@@ -6,8 +6,8 @@ function execute(url) {
     const match = url.match(regex);
     let book_id = match[1];
     console.log(book_id)
-    let response = fetch(BASE_URL + "/b/" + book_id +"/");
-    console.log(BASE_URL + "/b/" + book_id +"/")
+    let response = fetch(BASE_URL + "/book/" + book_id +"/");
+    console.log(BASE_URL + "/book/" + book_id +"/")
     if (response.ok) {
         let doc = response.html('gbk');
 
