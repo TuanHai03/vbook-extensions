0ecb5ec3da5e2d7c294087ed67bb21d664140d51
TuanHai03 <157023052+TuanHai03@users.noreply.github.com>
2025-02-08 21:49:32 +0700

    Add files via upload

--- Uaa/src/chap.js
+++ Uaa/src/chap.js
@@ -1,13 +1,25 @@
 load("config.js");
 function execute(url) {
-var browser = Engine.newBrowser() // Khởi tạo browser
-browser.setUserAgent(UserAgent.android())
-browser.launch(url,4000)
-sleep(4000)
-    let response = browser.html();
-//console.log(response)
-    if (response.select("body")!="") {
-        let doc = response; // Xử lý trang HTML
+
+let body={
+  method: "POST", // GET, POST, PUT, DELETE, PATCH
+  headers: {
+    "User-Agent": UserAgent.android()
+  }
+    }
+if(BASE_COOKIE!=""){
+    body={
+  method: "POST", // GET, POST, PUT, DELETE, PATCH
+  headers: {
+    "cookie": BASE_COOKIE,
+    "User-Agent": UserAgent.android()
+  }
+}
+}
+    let response = fetch(url,body);
+
+    if (response.ok) {
+        let doc = response.html(); // Xử lý trang HTML
         let content = "";
 console.log(doc)
         // Lấy các phần tử có class 'line' và nối thành nội dung
