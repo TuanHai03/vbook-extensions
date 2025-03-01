6cd64274cb43c7b0a8cfcd85fa8094384000884b
TuanHai03 <157023052+TuanHai03@users.noreply.github.com>
2025-03-01 22:01:46 +0700

    Add files via upload

--- Sangt̀acviet/src/chap.js
+++ Sangt̀acviet/src/chap.js
@@ -5,9 +5,14 @@ function execute(url) {
         let books=keys.json() 
         //return Response.success(books.data);
         try{
-return Response.success(books.data.replace(/<i([^>]*)t='(.*?)'([^>]*)>(.*?)<\/i>/g, `<i$1t='$2'$3>$2</i>`));
+return Response.success(books.data.replace(/<i([^>]*)t='(.*?)'([^>]*)>(.*?)<\/i>/g, `<i$1t='$2'$3>$2</i>`).replace("\n","<\/p>").replace("\t","<p>\t"));
         }catch(error){
-            return Response.success(books.data);
+            try{
+                return Response.success(books.data.replace("\n","<\/p>").replace("\t","<p>\t"));
+            }catch(error){
+                return Response.success(books.err)
+            }
+            
         }
          }
         return null;
